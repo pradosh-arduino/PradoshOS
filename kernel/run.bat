690b97dd60d767ef74500b50af512db43108649b
Pradosh S <69463173+pradosh-arduino@users.noreply.github.com>
2021-11-18 18:58:12 +0530

    Small change

--- kernel/run.bat
+++ kernel/run.bat
@@ -11,7 +11,5 @@ type %KERNELDIR%/cpu-print.txt
 set /p processor=Enter Processor to boot:
 set /p mem=Enter memory:
 
-cd C:\Program Files\
-cd qemu
 qemu-system-x86_64 -drive file=%BUILDDIR%/%OSNAME%.img -drive file=%BUILDDIR%/floppy.img -device nvme,serial=PradoshOS -m %mem% -cpu %processor% -drive if=pflash,format=raw,unit=0,file=%OVMFDIR%/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFDIR%/OVMF_VARS-pure-efi.fd -net none
 timeout 30
