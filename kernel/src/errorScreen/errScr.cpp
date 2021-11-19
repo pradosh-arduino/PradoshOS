8f71968da906be5a8b88c3bbc97d1c891c3eb5a5
Pradosh S <69463173+pradosh-arduino@users.noreply.github.com>
2021-11-19 11:12:12 +0530

    Update errScr.cpp

--- kernel/src/errorScreen/errScr.cpp
+++ kernel/src/errorScreen/errScr.cpp
@@ -3,11 +3,13 @@
 #include "../cstr.h"
 #include "../mtask.h"
 #include "../interrupts/IDT.h"
-IDTDescEntry* descE;
-void Panic(const char* msg, const char* location, const char* when, const char* Gate, uint8_t selector){
+
+void Panic(const char* msg, const char* location, const char* when, uint64_t offset, uint8_t type_attr, uint16_t selector){
     GlobalRenderer->ClearColour = 0x00eb3b3b;
     GlobalRenderer->Clear();
     GlobalRenderer->CursorPosition = {0, 0};
+
+    //uint16_t sel = selectorI;
 
     GlobalRenderer->Print("Critical System Error!");
     GlobalRenderer->Next();
@@ -17,10 +19,18 @@ void Panic(const char* msg, const char* location, const char* when, const char* 
     GlobalRenderer->Print("Error code:");
     GlobalRenderer->Next();
     GlobalRenderer->Print("0x");
-    GlobalRenderer->Print(to_hstring((uint16_t)descE->GetOffset()));
+    GlobalRenderer->Print(to_hstring((uint16_t)offset));
     GlobalRenderer->Next();
     GlobalRenderer->Print("Gate Type: ");
-    GlobalRenderer->Print(Gate);
+    if(type_attr == 0b10001110){
+        GlobalRenderer->Print("IDT_TA_InterruptGate");
+    }else if(type_attr == 0b10001100){
+        GlobalRenderer->Print("IDT_TA_CallGate");
+    }else if(type_attr == 0b10001111){
+        GlobalRenderer->Print("IDT_TA_TrapGate");
+    }else{
+        GlobalRenderer->Print("Unknown Gate - (Very Critical error)");
+    }
     GlobalRenderer->Next();
     GlobalRenderer->Print("Selector: 0x");
     GlobalRenderer->Print(to_hstring((uint8_t)selector));
