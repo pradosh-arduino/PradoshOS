690cce07ecf4e0dbd52d6a077586deb2da445b65
Pradosh S <69463173+pradosh-arduino@users.noreply.github.com>
2021-11-19 11:12:31 +0530

    Update errScr.h

--- kernel/src/errorScreen/errScr.h
+++ kernel/src/errorScreen/errScr.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <stdint.h>
 
-void Panic(const char* msg, const char* location, const char* when, const char* Gate, uint8_t selector);
+void Panic(const char* msg, const char* location, const char* when, uint64_t offset, uint8_t type_attr, uint16_t selector);
