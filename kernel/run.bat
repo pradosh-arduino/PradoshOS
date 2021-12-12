f40ac26cf3e827ae72878bb3c8bb036936ee68ce
pradosh-arduino <pradoshnathan@gmail.com>
2021-12-12 14:59:43 +0530

    Changed CustomOS to PradoshOS

--- kernel/run.bat
+++ kernel/run.bat
@@ -5,13 +5,11 @@ set KERNELDIR=%0/../
 
 set BUILDDIR=%BUILDDIR:"=%
 set OVMFDIR=%OVMFDIR:"=%
-set OSNAME=CustomOS
+set OSNAME=PradoshOS
+copy \\wsl$\Ubuntu-20.04\home\pradosh\PradoshOS-0.1.0-indev-uint8_t-error\PradoshOS-0.1.0-indev-uint8_t-error\PureBin\ \\wsl$\Ubuntu-20.04\home\pradosh\PradoshOS-0.1.0-indev-uint8_t-error\PradoshOS-0.1.0-indev-uint8_t-error\OVMFbin\
 cls
-type %KERNELDIR%/cpu-print.txt
-set /p processor=Enter Processor to boot:
-set /p mem=Enter memory:
 
 cd C:\Program Files\
 cd qemu
-qemu-system-x86_64 -drive file=%BUILDDIR%/%OSNAME%.img -drive file=%BUILDDIR%/floppy.img -device nvme,serial=PradoshOS -m %mem% -cpu %processor% -drive if=pflash,format=raw,unit=0,file=%OVMFDIR%/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFDIR%/OVMF_VARS-pure-efi.fd -net none
+qemu-system-x86_64 -drive file=%BUILDDIR%/%OSNAME%.img -drive file=%BUILDDIR%/floppy.img -rtc base=localtime,clock=host -device nvme,serial=PradoshOS -m 256M -cpu qemu64 -drive if=pflash,format=raw,unit=0,file=%OVMFDIR%/OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=%OVMFDIR%/OVMF_VARS-pure-efi.fd -net none
 timeout 30
