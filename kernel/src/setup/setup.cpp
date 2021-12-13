0d90f8efaeb80d9d17120e4d472164b74ec3458d
pradosh-arduino <pradoshnathan@gmail.com>
2021-12-13 17:29:48 +0530

    src clear

--- kernel/src/setup/setup.cpp
+++
@@ -1,9 +0,0 @@
-#include "../BasicRenderer.h"
-#include "setup.h"
-void InitializeSetup(){
-     GlobalRenderer->Clear();
-     GlobalRenderer->ClearColour = 0x00805080; // backround
-     GlobalRenderer->Clear();
-     GlobalRenderer->Print("Welcome to PradoshOS installation");
-     GlobalRenderer->Next();
-}
--- kernel/src/userland/userspace.c
+++
@@ -1,9 +0,0 @@
-#include "usergdt.h"
-#include "tss.h"
-#include "../BasicRenderer.h"
-
-uint64_t UserStack[0x400];
-
-void userFunction() {
-    while(1){}
-}
